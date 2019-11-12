3dd9ffd2c7da8bc31d4affc044711b7cee1d6d36
Rodrigo Neves <rd_neves_dark@hotmail.com>
2019-11-12 11:39:39 -0400

    Adicionado teste de adição e de consultas ao banco.

---
+++ ModuloAdministradorApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Logging;
+using ModuloAdministrador.Domain.Entities;
+using ModuloAdministrador.Infra.CrossCutting.Logging;
+using ModuloAdministrador.Infra.Data.Context;
+using System;
+using System.Linq;
+
+namespace ModuloAdministradorApp {
+    class Program {
+        static void Main(string[] args) {
+            Usuario usuario1;
+            Usuario usuario2;
+
+            Usuario CriarUsuario(String nome, String email, String cpf) {
+                return new Usuario() {
+                    Nome = nome,
+                    Email = email,
+                    Cpf = cpf
+                };
+            }
+
+            usuario1 = CriarUsuario("Mais Usuario", "xyz@efg.com", "000400");
+            usuario2 = CriarUsuario("Another Usuario", "xyz@efg.com", "020030");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ModuloAdministradorContext>();
+            optionsBuilder.UseLazyLoadingProxies();
+            optionsBuilder.UseMySql("Server=localhost; userid=root; password=admin; database=ModuloAdministradorDB;",
+                                    m => m.MigrationsAssembly("ModuloAdministrador.Infra.Data"));
+            try {
+                using (var dbcontext = new ModuloAdministradorContext(optionsBuilder.Options)) {
+                    dbcontext.GetService<ILoggerFactory>().AddProvider(new Logger());
+
+                    //dbcontext.Usuarios.Add(usuario1);
+                    //dbcontext.Usuarios.Add(usuario2);
+                    //dbcontext.SaveChanges();
+
+                    var resultado = dbcontext.Usuarios.Where(u => u.Id<3);
+
+                    foreach(var us in resultado) {
+
+                    }
+                }
+            }
+            catch(Exception e) {
+                Console.WriteLine("Erro: " + e.Message);
+                Console.ReadKey();
+
+            }
+            Console.WriteLine("Ok...");
+            Console.ReadKey();
+        }
+    }
+}
